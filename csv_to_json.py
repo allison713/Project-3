79ca85661b112636b98309be378079388cb0d123
Alejandro-Delacruz <118419015+Alejandro-Delacruz@users.noreply.github.com>
2023-04-11 19:51:04 -0500

    Test

---
+++ csv_to_json.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+data=pd.read_csv("complete.csv",on_bad_lines='skip')
+
+new_data={}
+for i in range(len(data.columns)):
+    new_data[data.columns[i]]=list(data[data.columns[i]])
+    
+
+import json
+
+# Serializing json
+json_object = json.dumps(new_data, indent=4)
+ 
+# Writing to sample.json
+with open("sample.json", "w") as outfile:
+    outfile.write(json_object)
