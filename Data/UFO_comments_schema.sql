90eaadd602cb0cca9a141777c74d684c0afef4d4
allison713 <allison.713@gmail.com>
2023-04-12 19:02:59 -0500

    updated flask + created tables in schema

--- Data/UFO_comments_schema.sql
+++ Data/UFO_comments_schema.sql
@@ -1,5 +1,6 @@
 --Drop tables
 DROP TABLE UFO_comments;
+DROP TABLE UFO_cleaned;
 
 --Create Tables for each CSV
 CREATE TABLE UFO_comments (
@@ -10,5 +11,20 @@ CREATE TABLE UFO_comments (
 	comments VARCHAR	
 );
 
+CREATE TABLE UFO_cleaned (
+ 	id INT NOT NULL PRIMARY KEY,
+  	date_ocurrence DATE,
+	city VARCHAR,
+	state VARCHAR(2),
+	country VARCHAR,
+	shape VARCHAR,
+	duration_seconds float,
+	duration_hours_min VARCHAR,
+	date_posted DATE,
+	latitude FLOAT,
+	longitude FLOAT
+);
+
 --Import data into tables
 COPY ufo_comments FROM  'C:\Users\Allison\Downloads\Project-3\UFO_comments.csv' WITH delimiter ','  CSV HEADER;
+COPY ufo_cleaned FROM  'C:\Users\Allison\Downloads\Project-3\UFO_cleaned.csv' WITH delimiter ','  CSV HEADER;
