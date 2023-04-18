bbe99847eb0382c00d3c110844a9789c72b6e5f9
allison713 <allison.713@gmail.com>
2023-04-17 19:58:19 -0500

    updated app.py

--- Final-Versions/app.py
+++ Final-Versions/app.py
@@ -40,17 +40,7 @@ def welcome():
         f"JSON list of real comments by date for the most recent years of the data available</a></li><br/>"
 
         f"<li><a href= 'http://127.0.0.1:5000/api/v1.0/ufo_sighting_data'>"
-        f"JSON list of sighting details by date and with locations</a></li><br/>"
-       
-        f"<li><a href= 'http://127.0.0.1:5000/api/v1.0/ufo_sighting_by_date'>"
-        f"JSON list of the last 12 months of recorded sightings</a></li><br/>"
-
-        f"<li><a href= 'http://127.0.0.1:5000/api/v1.0/ufo_shapes_reported_fequency'>"
-        f"This gives filtered informamtion of frequency by location</a></li><br/>"
-
-        f"<li><a href= 'http://127.0.0.1:5000/api/v1.0/other_ufo_filtered_data'>"
-        f"other ufo filtered data</a></li></ul><br/>"
-    )
+        f"JSON list of sighting details by date and with locations</a></li><br/>")
 
 
 
@@ -69,46 +59,11 @@ def comments():
 def cleaned():
     conn = get_db_connection()
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cur.execute('SELECT * FROM ufo_sightenings;')
+    cur.execute('SELECT * FROM ufo_sightings;')
     ufo = cur.fetchall()
     cur.close()
     conn.close()
     return jsonify(ufo)
-  
-#route that filters ufo_comments by location grouped by date
-@app.route("/api/v1.0/ufo_sighting_by_date")
-def ufo_sighting_by_date():
-    conn = get_db_connection()
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    #cur.execute('SELECT * FROM ufo_ufo_sightenings WHERE "state" GROUPED BY date_ocurrence ;')
-    cur.execute('SELECT * FROM ufo_sightenings WHERE "country" GROUPED BY date_ocurrence ;')
-    ufo_sighting_by_date = cur.fetchall()
-    cur.close()
-    conn.close()
-    return jsonify(ufo_sighting_by_date)
-   
-
-#@app.route(/ufo_sighting_by_date")
-#return()
-
-#@app.route(/ufo_shapes_reported_fequency)
-#return()
-
-#@app.route(/other_ufo_filtered_data)
-#return()
-#@app.route("/image.jpg")
-#def image():
-#    return render_template("image.jpg")
-
-@app.route("/<name>")
-def user(name):
-    return f"Hello-- {name}!"
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
