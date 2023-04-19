92e91b9b7a5bcab67ca91a668d5cab00a5dd26ec
allison713 <allison.713@gmail.com>
2023-04-18 19:36:39 -0500

    updated app to make bar chart

--- static/app.js
+++ static/app.js
@@ -1,113 +1,120 @@
 // json url
 const url = "http://127.0.0.1:5000/api/v1.0/ufo_sighting_data"
 
-// initialize function to pull ID numbers
-function main() {
-    var dropDown = d3.select("#selectedDataset");
-    // retrieve JSON data
-    d3.json(url).then(function (data) {
-        var sampleNames = d3.group(data, d=> d.date);
-        sampleNames.forEach((sample) => {
-            dropDown.append("option").text(sample.date.substring(0,4)).property("value", sample.date.substring(0,4))
-        });
-        // var initSample = sampleNames[0];
-        // demographicInfo(initSample);
-        // charts(initSample);
+
+d3.json(url).then(function (data) {
+
+    function init() {
+        var year = "1949"
+        var filtered = data.filter(row => row.date.substring(0,4) == year);
+        var states = [];
+
+        for (let i=0; i<data.length; i++) {
+            var state = data[i].state;
+            if (states.includes(state) === false) {
+                states.push(state);
+            }
+        }
+
+        let xChoice = states.map(x => x);
+
+        let stateCounts = [];
+
+        states.forEach(state => {
+            count = filtered.filter(row => row.state == state).length;
+            stateCounts.push(count);}
+        );
+
+        let trace = {
+            x: xChoice,
+            y: stateCounts,
+            type: "bar",
+            orientation: "v"
+        };
+
+        let traceData = [trace];
+
+        let layout = {
+            title: `UFO sightings per state in year 1949`,
+            autosize: false,
+            width: 1100,
+            height: 450
+          }
+
+        Plotly.newPlot("bar", traceData, layout);
+    };
+
+    //add each year to the dropdown.
+    var dropDown = d3.select("#selDataset");
+    var years = [];
+
+        for (let i=0; i<data.length; i++) {
+            var year = data[i].date.substring(0,4);
+            if (years.includes(year) === false) {
+                years.push(year);
+            }
+        }
+
+        years.forEach((year) => {
+            dropDown.append("option").text(year).property("value", year);
+            });
+
+    //Trigger to update the graph
+    d3.selectAll("#selDataset").on("change", function() {
+        updatePlotly();
     });
-};
 
-//--------------------------------------------------------------
-// Building a demographic metadata 
-//--------------------------------------------------------------//
-// function demographicInfo(sample) {
-//     var demo = d3.select("#sample-metadata");
-//     d3.json(url).then(function (data) {
-//         var metaData = data.matedata;
-//         var metaDataSample = metaData.filter(row => row.id == sample);
-//         demo.selectAll("p").remove();
-//         metaDataSample.forEach((row) => {
-//             for (const [key, value] of Object.entries(row)) {
-//                 demo.append("p").text(`${key.charAt(0).toUpperCase()+key.slice(1)}:   ${value}`);
-//             };
-//         });
-//     });
-// };
+    //Function to change graph when dropdown is selected
+    function updatePlotly() {
+        // Use D3 to select the drop down menu
+        let dropdownMenu = d3.select("#selDataset");
 
+        // Assign the value of the dropdown menu option to a variable
+        let name = dropdownMenu.property("value");
 
-//---------------------------------------------------
-//Building Chartes
-//---------------------------------------------------//
-// function charts(sample) {
-//     d3.json(url).then(function (data) {
-//         // variables for charts
-//         var samplesComplete = data.sample;
-//         var sampleInfo = samplesComplete.filter(row => row.id == sample);
-//         var sampleValues = sampleInfo[0].count;
-//         var otuIds = sampleInfo[0].state_name;
+        var year = `${name}`
+        var filtered = data.filter(row => row.date.substring(0,4) == year);
+        var states = [];
+        
+        for (let i=0; i<data.length; i++) {
+            var state = data[i].state;
+            if (states.includes(state) === false) {
+                states.push(state);
+            }
+        }
 
-//         var sampleshape = data.shape_sample;
-//         var sampleInfo2 = sampleshape.filter(row => row.id == sample);
-//         var sampleValues2 = sampleInfo2[0].count;
-//         var otuIds2 = sampleInfo2[0].state_name;
+        let xChoice = states.map(x => x);
 
-//         //----------------------------------------------
-//         // Building a bar chart
-//         //----------------------------------------------//
-//         var bardata = {
-//             y: sampleValues,
-//             x: otuIds,
-//             type: "bar",
-//             orientation: "v",
-//         };
-//         var data = [bardata];
-//         // var barlayout = { height: 400, margin: { t: 5, b: 0 }};
-//         var bartitle={title:"Number of UFO Observations by State"};
-//         Plotly.newPlot("bar", data,bartitle)
+        //Can't get the states to sort by Alpha!!//
+        xChoice.sort(function (a,b) {
+            return b-a;
+        });
 
-//         var bardata = {
-//             x: sampleValues2,
-//             y: otuIds2,
-//             type: "bar",
-//             orientation: "h",
-//         };
-//         var data = [bardata];
-//         // var barlayout = { height: 400, margin: { t: 5, b: 0 }};
-//         var bartitle={title:"Number of UFO Observations by Shape"};
-//         Plotly.newPlot("bar2", data,bartitle)
+        let stateCounts = [];
 
+        states.forEach(state => {
+            count = filtered.filter(row => row.state == state).length;
+            stateCounts.push(count);}
+        );
 
+            //update the bar chart
+            updateBar = {
+                x: xChoice,
+                y: stateCounts,
+                type: "bar",
+                orientation: "v"
+              };
 
-//         //---------------------------------------------------------
-//         // Building  a bubble chart
-//         //----------------------------------------------------------//
+            let trace = [updateBar];
+            let layout = {
+                title: `UFO sightings per state in the year ${year}`,
+                autosize: false,
+                width: 1100,
+                height: 450
+              }
 
-//         var firsttrace = {
-//             x: otuIds2,
-//             y: sampleValues2,
-//             mode: "markers",
-//             marker: {
-//                 size: sampleValues2,
-//                 color: otuIds2,
-//                 colorscale: "Earth"
-//             },
-//             text: otuIds2
-//         };
-//         var bubdata = [firsttrace];
-//         var bublayout = {title: "Number of obeservations per shape",
-//         xaxis: {title:"OTU ID"},
-//             showlegend: false
-//         };
-
-//         Plotly.newPlot("bubble", bubdata, bublayout);
-
-//     });
-// };
-
-
-// function optionChanged(sample) {
-//     demographicInfo(sample);
-//     charts(sample);
-// };
-
-// call main function to run
-main();
+            //new graph
+            Plotly.newPlot("bar", trace, layout);
+        };
+    init();
+    });
